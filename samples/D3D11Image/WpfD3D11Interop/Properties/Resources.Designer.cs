a8781be7d968f7c351d420809af8cdf46625afa9
andyb1979 <aburnettthompson@gmail.com>
2016-10-03 22:16:56 +0100

    Got it building

--- samples/D3D11Image/WpfD3D11Interop/Properties/Resources.Designer.cs
+++ samples/D3D11Image/WpfD3D11Interop/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Samples.Wpf.D3D11Interop.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("D3D11ImageSample.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Samples.Wpf.D3D11Interop.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
